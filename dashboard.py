f7b98b6bd24ad802fa8fccf3b3aff91876a35002
gabrielferrazz <gabriel.rferraz@hotmail.com>
2023-10-11 15:18:57 -0300

    dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,10 @@
-import pandas as pd # Pandas é uma biblioteca para manipulação e análise de dados.
-import dash # Dash é uma biblioteca que permite criar aplicativos web interativos e analíticos em Python
-from dash import html # 'html'- módulo específico da biblioteca Dash, que fornece componentes HTML.
-from dash import dcc # 'dcc'- fornece componentes mais avançados e interativos, como gráficos interativos, botões, menus suspensos, caixas de seleção e muito mais. 
+# Pandas é uma biblioteca para manipulação e análise de dados.
+import pandas as pd
+import dash  # Dash é uma biblioteca que permite criar aplicativos web interativos e analíticos em Python
+# 'html'- módulo específico da biblioteca Dash, que fornece componentes HTML.
+from dash import html
+# 'dcc'- fornece componentes mais avançados e interativos, como gráficos interativos, botões, menus suspensos, caixas de seleção e muito mais.
+from dash import dcc
 from dash.dependencies import Input, Output
 
 # Leitura dos dados do arquivo Excel
@@ -10,11 +13,11 @@ tipos_licenca = df.columns[1:]
 centros_custo = df['Centro de Custo'].unique()
 
 # Cores para a página
-cor_de_fundo = '#181818'       # Cor de fundo Cinza escuro
-cor_principal_amarelo = '#fca311'      # Amarelo
-cor_principal_branco = '#ffffff'      # Branco
-cor_secundaria = '#FFA500'     # Laranja
-cor_fonte = '#ffffff'          # Branco
+cor_de_fundo = '#E6E6FA'       # Cor de fundo Azul claro
+cor_principal_amarelo = '#7B68EE'      # Azul
+cor_principal_branco = '#000000'      # Preto
+cor_secundaria = '#7B68EE'     # Azul
+cor_fonte = '#000000'          # Preto
 cor_rodape = '#000000'         # Preto
 
 # Estilos CSS personalizados
@@ -25,7 +28,8 @@ styles = {
     'padding': '10px',
     'color': cor_rodape,
     'border-radius': '10px',
-    'box-shadow': '0 4px 8px 0 rgba(0,0,0,0.2)',  # Adicionando sombra para destacar o dashboard
+    # Adicionando sombra para destacar o dashboard
+    'box-shadow': '0 4px 8px 0 rgba(0,0,0,0.2)',
 }
 
 # Estilos CSS personalizados
@@ -36,13 +40,14 @@ styles_resultados = {
     'padding': '10px',
     'color': cor_fonte,
     'border-radius': '10px',
-    'box-shadow': '0 4px 8px 0 rgba(0,0,0,0.2)',  # Adicionando sombra para destacar o dashboard
+    # Adicionando sombra para destacar o dashboard
+    'box-shadow': '0 4px 8px 0 rgba(0,0,0,0.2)',
 }
 
 # Estilos CSS para o título amarelo
 titulo_styles_amarelo = {
     'text-align': 'center',
-    'font-size': '50px',
+    'font-size': '45px',
     'font-family': 'Arial, sans-serif',
     'color': cor_principal_amarelo,
     'margin-top': '30px',  # Adicionando espaço entre o título e os dropdowns
@@ -51,7 +56,7 @@ titulo_styles_amarelo = {
 # Estilos CSS para o título branco
 titulo_styles_branco = {
     'text-align': 'center',
-    'font-size': '50px',
+    'font-size': '45px',
     'font-family': 'Arial, sans-serif',
     'color': cor_principal_branco,
     'margin-top': '30px',  # Adicionando espaço entre o título e os dropdowns
@@ -76,68 +81,69 @@ app.layout = html.Div(
     style={'background-color': cor_de_fundo, 'min-height': '100vh'},
     children=[
         html.Div(
-            style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center'},
+            style={'display': 'flex', 'justify-content': 'center',
+                   'align-items': 'center'},
             children=[
-                html.H1("Gabe`s", style=titulo_styles_amarelo,),
+                html.H1("Ferra'z", style=titulo_styles_amarelo,),
                 html.H1(".", style=titulo_styles_amarelo,),
-                html.H1("Rate", style=titulo_styles_branco,),
+                html.H1("Dash.365", style=titulo_styles_branco,),
             ]
         ),
 
         dcc.Dropdown(
             id='tipo-licenca-dropdown',
-            options=[{'label': licenca, 'value': licenca} for licenca in tipos_licenca],
+            options=[{'label': licenca, 'value': licenca}
+                     for licenca in tipos_licenca],
             value=tipos_licenca[0],
             style=styles
         ),
 
         dcc.Dropdown(
             id='centro-custo-dropdown',
-            options=[{'label': centro, 'value': centro} for centro in centros_custo],
+            options=[{'label': centro, 'value': centro}
+                     for centro in centros_custo],
             value=centros_custo[0],
             style=styles
         ),
 
         html.Div(id='detalhes-licencas', style=styles),
-
-        html.Div(
-            style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center', 'margin-top': '60px'},
-            children=[
-                html.Button('TOTVS', id = 'TOTVS', n_clicks=0, style={'margin-right': '10px'}),
-                html.Button('OFFICE 365', id = 'OFFICE 365', n_clicks=0, style={'margin-left': '10px'})
-
-            ]
-        ),
-
     ]
 )
 
 # Callback para atualizar a tabela com base nas seleções do usuário
+
+
 @app.callback(
     Output('detalhes-licencas', 'children'),
     [Input('tipo-licenca-dropdown', 'value'),
      Input('centro-custo-dropdown', 'value')]
 )
 def update_table(tipo_licenca, centro_custo):
-    filtered_df = df[(df['Centro de Custo'] == centro_custo) & (df[tipo_licenca] > 0)]
+    filtered_df = df[(df['Centro de Custo'] == centro_custo)
+                     & (df[tipo_licenca] > 0)]
 
     if filtered_df.empty:
         return html.Table([
-            html.Thead(html.Tr([html.Th("Centro de Custo"), html.Th("Quantidade Usada", style={'text-align': 'center'})])),
-            html.Tbody(html.Tr([html.Td(centro_custo), html.Td(0, style={'text-align': 'center'})]))
+            html.Thead(html.Tr([html.Th("Centro de Custo"), html.Th(
+                "Quantidade Usada", style={'text-align': 'center'})])),
+            html.Tbody(
+                html.Tr([html.Td(centro_custo), html.Td(0, style={'text-align': 'center'})]))
         ], style=styles_resultados)
 
     tabela = html.Table([
         html.Thead(
-            html.Tr([html.Th("Centro de Custo"), html.Th("Quantidade Usada", style={'text-align': 'center'})])
+            html.Tr([html.Th("Centro de Custo"), html.Th(
+                "Quantidade Usada", style={'text-align': 'center'})])
         ),
         html.Tbody([
-            html.Tr([html.Td(filtered_df.iloc[i]['Centro de Custo']), html.Td(filtered_df.iloc[i][tipo_licenca], style={'text-align': 'center'})]) 
+            html.Tr([html.Td(filtered_df.iloc[i]['Centro de Custo']), html.Td(
+                filtered_df.iloc[i][tipo_licenca], style={'text-align': 'center'})])
             for i in range(filtered_df.shape[0])
         ])
     ], style=styles_resultados)
 
     return tabela
+
 
 # Execução do aplicativo Dash
 if __name__ == '__main__':
